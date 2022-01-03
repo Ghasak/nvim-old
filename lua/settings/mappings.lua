0aa7977605f470fc257d10fb48917764deadd8d3
Ghasak <ghasak.ibrahim@jp.ey.com>
2022-01-03 15:56:34 +0900

    [v02-02] supporting now for NVIM v.06

--- lua/settings/mappings.lua
+++ lua/settings/mappings.lua
@@ -4,7 +4,6 @@
 -- 1.) Global optoins (vim.o)
 -- 2.) Local to window (vim.wo)
 -- 3.) Local to buffer (vim.bo)
-
 -- Adding some configurations
 -- How to know to which configuration use :h expandtab
 -- My Leader key
@@ -16,7 +15,10 @@ vim.g.mapleader = " "
 -- Set using (vim.api.nvim_set_keymap({mode}, {keymap}, {mapped to}, {options}))
 -- Using Ctrl-s to save
 local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local opts = {
+	noremap = true,
+	silent = true,
+}
 
 keymap("n", "<c-s>", ":w<CR>", {})
 keymap("i", "<c-s>", "<Esc>:w<CR>a", {})
@@ -41,44 +43,90 @@ noremap <silent> <C-j> <c-w>j
 
 ]])
 -- Explorer with Nvim-tree( ensure first the undotree is not toggled )
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>e",
-	":<cmd>:UndotreeHide<CR>:NvimTreeToggle<CR>",
-	{ noremap = true, silent = true }
-)
+vim.api.nvim_set_keymap("n", "<Leader>e", ":<cmd>:UndotreeHide<CR>:NvimTreeToggle<CR>", {
+	noremap = true,
+	silent = true,
+})
 -- Remove highlighting after searching
-vim.api.nvim_set_keymap("n", "<Leader>h", ":nohlsearch<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<Leader>vh", ":nohlsearch<CR>", { noremap = true, silent = true })
---vim.api.nvim_set_keymap('n','<Leader>h',':set hlsearch!<CR>',{noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>h", ":nohlsearch<CR>", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("v", "<Leader>vh", ":nohlsearch<CR>", {
+	noremap = true,
+	silent = true,
+})
+-- vim.api.nvim_set_keymap('n','<Leader>h',':set hlsearch!<CR>',{noremap = true, silent = true})
 -- Setting for the space key
---vim.api.nvim_set_keymap('n','<Leader>','<NOP>',{noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('n','<Leader>','<NOP>',{noremap = true, silent = true})
 
 -- Better indenting using (<) and (>)
-vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<", "<gv", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("v", ">", ">gv", {
+	noremap = true,
+	silent = true,
+})
 
 -- Alternative to Escape Key
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("i", "kj", "<Esc>", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", {
+	noremap = true,
+	silent = true,
+})
 
 -- Tab switch buffers
-vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", {
+	noremap = true,
+	silent = true,
+})
 
 -- Move/selected line / block of text in visual mode
-vim.api.nvim_set_keymap("x", "K", ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "J", ":move '>+2<CR>gv-gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "K", ":move .-2<CR>==", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "J", ":move .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "K", ":move '<-2<CR>gv-gv", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("x", "J", ":move '>+2<CR>gv-gv", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("n", "K", ":move .-2<CR>==", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("n", "J", ":move .+1<CR>==", {
+	noremap = true,
+	silent = true,
+})
 
 -- yank to the end of a line
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>y", "y$", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "Y", "y$", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("n", "<leader>y", "y$", {
+	noremap = true,
+	silent = true,
+})
 
 local map = vim.api.nvim_set_keymap
-local default_opts = { noremap = true, silent = true }
+local default_opts = {
+	noremap = true,
+	silent = true,
+}
 -- close all windows and exit from neovim
 map("n", "<leader>q", ":qa<CR>", default_opts)
 
@@ -106,11 +154,22 @@ map("n", "<Leader>r", ":RnvimrToggle<CR>", default_opts)
 map("n", "<Leader>t", ":FloatermToggle<CR>", default_opts)
 
 -- Better nav for omni-complete
-vim.api.nvim_set_keymap("i", "<c-j>", '("<C-n>")', { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<c-k>", '("<C-p>")', { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<c-j>", '("<C-n>")', {
+	noremap = true,
+	silent = true,
+	expr = true,
+})
+vim.api.nvim_set_keymap("i", "<c-k>", '("<C-p>")', {
+	noremap = true,
+	silent = true,
+	expr = true,
+})
 
 -- Show definition of function based on nvim-lspConfig instead of K
-vim.api.nvim_set_keymap("n", "<F1>", ":lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F1>", ":lua vim.lsp.buf.hover()<CR>", {
+	noremap = true,
+	silent = true,
+})
 
 -- Using undo-tree, all the  changes to yoooour current buffer
 
@@ -122,18 +181,16 @@ vim.api.nvim_set_keymap("n", "<F1>", ":lua vim.lsp.buf.hover()<CR>", { noremap =
 
 -- This will ensure that you first untoggle the nvim-tree then toggle the undotree always
 -- this will be source with setting directory which shoould be put at the end of the loaded dictionaries
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>u",
-	":<cmd>NvimTreeClose<CR>:UndotreeToggle<CR>",
-	{ noremap = true, silent = true }
-)
+vim.api.nvim_set_keymap("n", "<leader>u", ":<cmd>NvimTreeClose<CR> :UndotreeToggle<CR>", {
+	noremap = true,
+	silent = true,
+})
 
 -- Spell Checking fomr coc
---vim.api.nvim_set_keymap('x','<leader>a',':<Plug>(coc-codeaction-selected',{noremap = true, silent = true})
---vim.api.nvim_set_keymap('','','',{noremap = true, silent = true})
---vim.api.nvim_set_keymap('','','',{noremap = true, silent = true})
---vim.api.nvim_set_keymap('','','',{noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('x','<leader>a',':<Plug>(coc-codeaction-selected',{noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('','','',{noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('','','',{noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('','','',{noremap = true, silent = true})
 -- This configuration was to use with coc-spell
 -- vim.cmd([[
 -- vmap <leader>a <Plug>(coc-codeaction-selected)
@@ -142,23 +199,32 @@ vim.api.nvim_set_keymap(
 -- ]])
 
 -- Source our init.lua file
-vim.api.nvim_set_keymap("n", "<space><CR>", ":source ~/.config/nvim/init.lua<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<space><CR>", ":source ~/.config/nvim/init.lua<CR>", {
+	noremap = true,
+	silent = true,
+})
 
 -- Maximize and minimize windows
-vim.api.nvim_set_keymap("n", "<space>+", ":vertical resize +5<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<space>-", ":vertical resize -5<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<space>+", ":vertical resize +5<CR>", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("n", "<space>-", ":vertical resize -5<CR>", {
+	noremap = true,
+	silent = true,
+})
 
 -- Allow to navigate the path (intellisense with the directory)
---vim.api.nvim_set_keymap('i','/','/<C-x><C-f>',{noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('i','/','/<C-x><C-f>',{noremap = true, silent = true})
 vim.cmd([[let g:clap_theme = 'material_design_dark']])
---vim.cmd[[autocmd FileType clap_input call compe#setup({ 'enabled': v:false }, 0)]]
---vim.cmd[[autocmd FileType clap_input let g:completion_enable_auto_pop = 0]]
---vim.cmd[[let g:clap_layout = { 'relative': 'editor' }]]
---vim.cmd[[let g:clap_layout = {'width': '10%', 'height': '13%', 'row': '33%', 'col': '17%'}]]
---vim.api.nvim_set_keymap('i','<leader>cf',"<cmd>Clap filer<CR>",{noremap = true, silent = true})
+-- vim.cmd[[autocmd FileType clap_input call compe#setup({ 'enabled': v:false }, 0)]]
+-- vim.cmd[[autocmd FileType clap_input let g:completion_enable_auto_pop = 0]]
+-- vim.cmd[[let g:clap_layout = { 'relative': 'editor' }]]
+-- vim.cmd[[let g:clap_layout = {'width': '10%', 'height': '13%', 'row': '33%', 'col': '17%'}]]
+-- vim.api.nvim_set_keymap('i','<leader>cf',"<cmd>Clap filer<CR>",{noremap = true, silent = true})
 
 -- replace with the register (you need a plugin  vim-scripts/replacewithregister)
---vim.api.nvim_set_keymap('n','<leader>cr','<Plug>replacewithregisteroperator',{noremap  = true, silent = true})
+-- vim.api.nvim_set_keymap('n','<leader>cr','<Plug>replacewithregisteroperator',{noremap  = true, silent = true})
 -- vim.api.nvim_set_keymap('n','<leader>crr','<plug>replacewithregisterline',{noremap  = true, silent = true})
 -- vim.api.nvim_set_keymap('x','<leader>cr','<plug>replacewithregistervisual',{noremap  = true, silent = true})
 -- vim.cmd([[
@@ -167,13 +233,21 @@ vim.cmd([[let g:clap_theme = 'material_design_dark']])
 --     xmap <leader>cr  <Plug>replacewithregistervisual
 -- ]])
 
-
 -- using vista with clap to see all the functions definitions and their implementation,
-vim.api.nvim_set_keymap("n", "<leader>vt", "<cmd>clap tags<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<leader>vt", "<cmd>clap tags<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>vt", "<cmd>clap tags<cr>", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("i", "<leader>vt", "<cmd>clap tags<cr>", {
+	noremap = true,
+	silent = true,
+})
 
 -- close windows without affecting other buffers
-vim.api.nvim_set_keymap("n", "<leader>w", "<cmd>Bdelete<cr>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<leader>w", "<cmd>Bdelete<cr>", {
+	noremap = true,
+	silent = false,
+})
 
 --  mapping j and k with  vim plugin acceleration (need rhysd/accelerated-jk plugin)
 local vim = vim
@@ -181,17 +255,17 @@ local t = function(str)
 	return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
--- _G.enhance_jk_move = function(key)
--- 	if packer_plugins["accelerated-jk"] and not packer_plugins["accelerated-jk"].loaded then
--- 		vim.cmd([[packadd accelerated-jk]])
--- 	end
--- 	local map = key == "j" and "<Plug>(accelerated_jk_gj)" or "<Plug>(accelerated_jk_gk)"
--- 	return t(map)
--- end
--- vim.cmd([[
--- nmap j <plug>(accelerated_jk_gj_position)
--- nmap k <plug>(accelerated_jk_gk_position)
---]])
+_G.enhance_jk_move = function(key)
+	if packer_plugins["accelerated-jk"] and not packer_plugins["accelerated-jk"].loaded then
+		vim.cmd([[packadd accelerated-jk]])
+	end
+	local map = key == "j" and "<Plug>(accelerated_jk_gj)" or "<Plug>(accelerated_jk_gk)"
+	return t(map)
+end
+vim.cmd([[
+ nmap j <plug>(accelerated_jk_gj_position)
+ nmap k <plug>(accelerated_jk_gk_position)
+]])
 --
 -- open a link in vim in browser: in linux use : xdg-open instead of open (for mac).
 -- source: " https://stackoverflow.com/questions/9458294/open-url-under-cursor-in-vim-with-browser "
@@ -231,8 +305,14 @@ endfun
 ]])
 
 -- Move to beginning and end of line in normal mode
-vim.api.nvim_set_keymap("n", "<leader>h", "<ESC>^", { noremap = true, silent = false })
-vim.api.nvim_set_keymap("n", "<leader>l", "<ESC>$", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<leader>h", "<ESC>^", {
+	noremap = true,
+	silent = false,
+})
+vim.api.nvim_set_keymap("n", "<leader>l", "<ESC>$", {
+	noremap = true,
+	silent = false,
+})
 
 -- Sending code to the jupyternotebook
 vim.cmd([[
